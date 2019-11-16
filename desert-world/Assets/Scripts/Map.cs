f88efd423c3af10d7b353bb6cfb7cc0815d17d95
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-11-16 11:04:41 +0000

    initial commit

---
+++ desert-world/Assets/Scripts/Map.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject PrefTile;
+    public int width = 3;
+    public int height = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateMap();
+    }
+
+    public void CreateMap() {
+        for (int w = -width; w <= width; w++) {
+            for (int h = -height; h <= height; h++) {
+                GameObject t = Instantiate(PrefTile, transform);
+                t.name = "tile " + w + " " + h;
+                t.GetComponent<Tile>().X = w;
+                t.GetComponent<Tile>().Y = h;
+                t.transform.SetPositionAndRotation(new Vector2(w, h), Quaternion.identity);
+            }
+        }
+    }
+
+    public Tile[] GetNeightbours(int x, int y) {
+        Tile[] found = new Tile[8];
+        foreach (GameObject c in transform) {
+            Tile t = c.GetComponent<Tile>();
+
+            // up
+            // down
+            // left
+            // right
+            // upright
+            // downright
+            // downleft
+            // upleft
+        }
+        return found;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ desert-world/Assets/Scripts/Tile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    public int X;
+    public int Y;
+    public Sprite spriteBarren;
+    public Sprite spriteFertile;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        GetComponent<SpriteRenderer>().sprite = spriteBarren;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

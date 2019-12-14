6957e222f416a63da5b8cd4831ff571c2f122d35
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-12-14 14:26:39 +0000

    plant humidity wip, need to refactor tilegetter

--- desert-world/Assets/Scripts/Plant.cs
+++ desert-world/Assets/Scripts/Plant.cs
@@ -14,31 +14,35 @@ public class Plant : MonoBehaviour
     public float Age = 0f;
     public Map Map;
     public float SeedRange;
+    private float localHumidity;
 
     void Start ()
     {
         rend = GetComponentInChildren<SpriteRenderer>();
         rend.sprite = Seed;
         Map = GameObject.Find("Map").GetComponent<Map>();
+        localHumidity = Random.Range(0f, 1f);
     }
 
     public void Step()
     {
         Age++;
-        if (Age == 4f) {
+        if (Age == 4f  && localHumidity >= 0.75f) {
             Map.SpawnPlant(transform, SeedRange);
-            //Map.SpawnPlant(transform, SeedRange);
-            //Map.SpawnPlant(transform, SeedRange);
         }
-        if (Age == 5f) Destroy(gameObject);
+        if (Age == 4f && localHumidity >= 0.85f) {
+            Map.SpawnPlant(transform, SeedRange);
+        }
+        if (Age == 4f && localHumidity >= 0.95f) {
+            Map.SpawnPlant(transform, SeedRange);
+        }
+        if (Age == 5f || localHumidity <= 0.05f) gameObject.SetActive(false);
     }
 
     private void Update () {
-        if (Age == 1f) rend.sprite = PlantS;
-        if (Age == 2f) rend.sprite = PlantM;
-        if (Age == 3f) rend.sprite = PlantFruited;
-        if (Age == 4f) {
-            rend.sprite = PlantWithered;
-        }
+        if (Age == 1f && localHumidity >= 0.25f) rend.sprite = PlantS;
+        if (Age == 2f && localHumidity >= 0.5f) rend.sprite = PlantM;
+        if (Age == 3f && localHumidity >= 0.75f) rend.sprite = PlantFruited;
+        if (Age == 4f || localHumidity < 0.25f) rend.sprite = PlantWithered;
     }
 }
